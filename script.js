418d76c5a56c8a6dfa718185800c3720526d219d
Pickled_Onion <ksalvaris@gmail.com>
2023-07-24 21:52:48 +0800

    updated css for search section and commenced javascript incorporating open weather API

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// import {weatherAPI} from './api_key.js';
 
-const weatherAPI = require('./api_key.js');
-console.log(weatherAPI); // Output: "Hello from file1.js!"
+// console.log(weatherAPI); // Output: "Hello from file1.js!"
+
+// document.getElementById("searchForm").addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     const city = document.getElementById("cityInput").value;
+
+//     const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&appid=75f727c580049b6c9fecb8d9f5740286';
+
+//     fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data);
+//         const weatherDataDiv = document.querySelector(".weather-data");
+//         weatherDataDiv.innerHTML = " ";
+//     })
+//     .catch((error) => {
+//         console.error("Error fetching data:", error);
+//     });
+// });
+
+var city = 'perth';
+const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&appid=75f727c580049b6c9fecb8d9f5740286';
+
+fetch(apiUrl)
+.then((response) => response.json())
+.then((data) => {
+    console.log(data);
+    // const weatherDataDiv = document.querySelector(".weather-data");
+    // weatherDataDiv.innerHTML = " ";
+})
+.catch((error) => {
+    console.error("Error fetching data:", error);
+});
+
+
