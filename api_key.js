1c7d9a7b8efb0a60a268d811637f69825caad30c
Alexander Babic <alexander.t.b@hotmail.com>
2023-07-31 19:22:17 +0800

    add final comments to JS code, remove keys from work, update to html modal buttons, final pass over JS code

--- api_key.js
+++ api_key.js
@@ -1,5 +1,5 @@
 
 // api_key.js 
-const weatherAPI = "75f727c580049b6c9fecb8d9f5740286"; 
-const Auth = 'Basic Qk9PVF83X1hYOkdGUm9ZV1dETUhrSw==';
-const MovieAPIKey = '7m4lC1r8ie30pB6qkMYMt8ciN8UrMCI36klQhSyD';
+const weatherAPI = ""; 
+const Auth = '';
+const MovieAPIKey = '';
